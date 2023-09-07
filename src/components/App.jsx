8801f62e9b46bcadffcbc0faa9649cc91a78ccae
arhistratig65 <118848591+arhistratig65@users.noreply.github.com>
2023-09-08 01:08:34 +0300

    componentDidUpdate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,31 +10,40 @@ const LS_KEY = 'contacts';
 export class App extends Component {
   state = {
     contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Jack Jones', number: '975-24-89' },
-      // { id: 'id-5', name: 'Captain Morgan', number: '227-90-26' },
-      // { id: 'id-6', name: 'Bilie Joe', number: '217-95-26' },
-      // { id: 'id-7', name: 'Joe Foster', number: '297-99-26' },
-      // { id: 'id-8', name: 'Samuel El Jackson', number: '287-04-26' },
-      // { id: 'id-9', name: 'Annie Milligan', number: '154-44-28' },
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Jack Jones', number: '975-24-89' },
+      { id: 'id-5', name: 'Captain Morgan', number: '227-90-26' },
+      { id: 'id-6', name: 'Bilie Joe', number: '217-95-26' },
+      { id: 'id-7', name: 'Joe Foster', number: '297-99-26' },
+      { id: 'id-8', name: 'Samuel El Jackson', number: '287-04-26' },
+      { id: 'id-9', name: 'Annie Milligan', number: '154-44-28' },
     ],
     filter: '',
   };
 
   componentDidMount() {
-    const contacts = localStorage.getItem(LS_KEY);
-    const parseContacts = JSON.parse(contacts);
-    if (parseContacts && parseContacts.length !== 0) {
-      console.log(parseContacts.length);
+    const contactsJson = localStorage.getItem('contacts');
+
+    if (contactsJson) {
+      const parseContacts = JSON.parse(contactsJson);
       this.setState({ contacts: parseContacts });
-    } else {
-      localStorage.removeItem(LS_KEY);
     }
   }
 
-  componentDidUpdate(prevProps, prevState) {
+  // componentDidMount() {
+  //   const contacts = localStorage.getItem(LS_KEY);
+  //   const parseContacts = JSON.parse(contacts);
+  //   if (parseContacts && parseContacts.length !== 0) {
+  //     console.log(parseContacts.length);
+  //     this.setState({ contacts: parseContacts });
+  //   } else {
+  //     localStorage.removeItem(LS_KEY);
+  //   }
+  // }
+
+  componentDidUpdate(_, prevState) {
     if (this.state.contacts !== prevState.contacts) {
       localStorage.setItem(LS_KEY, JSON.stringify(this.state.contacts));
     }
